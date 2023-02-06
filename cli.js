1ecf88e5269b4180914263347cc76858dc631ad5
Blockzilla101 <blockzilla101@gmail.com>
2023-02-06 20:04:22 +0500

    finish cli

--- cli.js
+++ cli.js
@@ -1,13 +1,37 @@
 const { Citation }= require('./index.js')
 const args = require('args-parser')(process.argv);
 
-if (args.output == null) {
+if (typeof args.output !== 'string') {
     console.log('output path is required')
     process.exit(1)
 }
 
+if (typeof args.reason !== 'string') {
+    console.log('reason is required')
+    process.exit(1)
+}
+
+if (typeof args.title !== 'string') {
+    console.log('title is required')
+    process.exit(1)
+}
+
+if (typeof args.penalty !== 'string') {
+    console.log('penalty is required')
+    process.exit(1)
+}
+
+
 const citation = new Citation(args.width, args.height);
-citation.resizeReason = true
-citation.resizeLimit = 100000
-citation.reason = 'blah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\nblah\n'
-citation.render(args.output, true)
+
+citation.resizeReason = args.resize ?? false
+citation.resizeLimit = args['resize-limit'] ?? 0
+citation.title = args.title
+citation.reason = args.reason
+citation.penalty = args.penalty
+
+if (args.barcode) {
+    citation.barcode = args.barcode.split('')
+}
+
+citation.render(args.output, args.gif)
